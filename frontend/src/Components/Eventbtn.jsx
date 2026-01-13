1d23404bf0b455f31da8730801e9e84b3a2b395c
Abhishek Yadav <yabhishekk480@gmail.com>
2026-01-13 23:27:45 +0530

    Btn designed

--- frontend/src/Components/Eventbtn.jsx
+++ frontend/src/Components/Eventbtn.jsx
@@ -10,8 +10,9 @@ const   Eventbtn = () => {
       }
 
   return  <div className="flex items-center justify-center mb-6  shadow-amber-100 " >
-   <button  onClick={btn}  className="  bg-pink-300  p-3 md:text-xl rounded-2xl  md:w-1/4  " >View All Events</button>
+   <button  onClick={btn}  className=" hover:scale-x-90 bg-pink-300  p-3 md:text-xl rounded-2xl  md:w-1/4  " >View All Events</button>
    </div> 
 };
 
 export default Eventbtn;
+  
