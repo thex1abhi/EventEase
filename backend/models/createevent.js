9fa5d4d9223013a6aee69afdf8ec0ced87382228
Abhishek Yadav <yabhishekk480@gmail.com>
2026-01-14 21:13:42 +0530

    Created Event Model

---
+++ backend/models/createevent.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const EventSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        City: {
+            type: String,
+            required: true
+        },
+        address: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: String,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true
+        },
+
+    }
+);
+
+export default mongoose.model("Event", EventSchema)
