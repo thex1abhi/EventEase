7c814cb5138123c5e1064d13ff331a78edb675a1
Abhishek Yadav <yabhishekk480@gmail.com>
2026-01-14 21:16:37 +0530

    Created Route and Form

--- backend/routes/organizer.routes.js
+++ backend/routes/organizer.routes.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { organizerlogin, organizersignup } from "../Controllers/Organizer.controller.js";
+import EventController from "../Controllers/EventController.js";
 
 const organizerroutes=express.Router();
  
 organizerroutes.post('/organizer-signup',organizersignup)
 organizerroutes.post('/organizer-login',organizerlogin);
-
-export default organizerroutes;
+organizerroutes.post('/create-event',EventController)
+export default organizerroutes; 
--- frontend/Organizer/Createevent.jsx
+++ frontend/Organizer/Createevent.jsx
@@ -1,66 +1,83 @@
-import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAppContext } from "../Context/AppContext";
+import { useState } from "react";
+import axios from "axios"
+import { toast } from "react-toastify";
 
-const Createevent = () => { 
-    const [eventname, setEventname] = useState("");
-    const [location, setlocation] = useState("");
-    const [date, setdate] = useState("");
-    const [time, settime] = useState("");
-    const [typeofEvent, setTypeofEvent] = useState("");
-      
-    const hanleEventCreation =async(e)=>{
-        e.preventDefault();
-            console.log("Creating Eveent");
-            
+const Createevent = () => {
+
+  const { backendurl } = useAppContext();
+  const navigate = useNavigate()
+
+
+  const [formData, setFormData] = useState({
+    name: "",
+    city: "",
+    address: "",
+    date: "",
+    type: "",
+    image: null,
+  });
+
+  const handleChange = (e) => {
+    if (e.target.name === "image") {
+      setFormData({ ...formData, image: e.target.files[0] })
+    } else {
+      setFormData({
+        ...formData, [e.target.name]: e.target.value
+      });
+
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    Object.keys(formData).forEach((key)=>data.append(key, formData[key]))
+
+
+    try {
+      const response = await axios.post(`${backendurl}/create-event"`, data, {
+        headers: { "content-type": "multipart/form-data" }
+      })
+      if (response.success === true) {
+        toast.success("Event created ")
+        navigate("/events")
+
+      }
+
+    } catch (error) {
+      console.log(error)
+      toast.error("Error Creating Event")
     }
 
-  return <> 
-  <div className=" m-10  flex items-center justify-center " > 
-    <form type="submit" className="space-y-3 w-2/4 "> 
-      <input
-        type="text"
-        placeholder="Enter Event Name"
-        name={eventname}
-        // onChange={e.target.value}
-        className="w-full px-4 py-2 border border-gray-200 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-300"
-      />
+  }
 
-      <input
-        type="text"
-        placeholder="Enter Event Location"
-        name={Location}
-        // onChange={e.target.value}
-        className="w-full px-4 py-2 border border-gray-200 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-300"
-      />
 
-      <input
-        type="date"
-     
-        name={date}
-        // onChange={e.target.value} 
-           placeholder="Enter Event Date"
-        className="w-full px-4 py-2 border border-gray-200 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-300"
-      />
 
-      <input
-        type="time"
-        placeholder="Enter Event Time"
-        name={time}
-        // onChange={e.target.value}
-        className="w-full px-4 py-2 border border-gray-200 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-300"
-      />  
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
 
-      <input
-        type="text"
-        placeholder="Enter Event Type"
-        name={typeofEvent}
-        // onChange={e.target.value}
-        className="w-full px-4 py-2 border border-gray-200 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-300"
-      /> 
+        <h2> Create Event  </h2>
+        <input type="text " name="name " onChange={handleChange} placeholder="Event Name" required />
 
-<button onSubmit={hanleEventCreation } > Create Event </button>
-    </form>
-  </div>
-  </>
-};
+        <input type="text " name="city" onChange={handleChange} placeholder="Event City" required />
+
+        <input type="text " name="address" onChange={handleChange} placeholder="Event Address" required />
+
+        <input type="date " name="date" onChange={handleChange} placeholder="Event Date" required />
+
+        <input type="text " name="type" onChange={handleChange} placeholder="Event Type" required />
+
+
+        <input type="file" name="image" onChange={handleChange} required />
+
+        <button type="submit"> Create Event  </button>
+        <button onClick={() => setFormData("")} >  </button>
+      </form>
+    </>
+  )
+}
 
 export default Createevent;
