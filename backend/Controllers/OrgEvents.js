fe36bb3433b6e54c3f7e00b3a2f578c19779cfd4
Abhishek Yadav <yabhishekk480@gmail.com>
2026-01-17 23:04:11 +0530

    Get  Org events By OrgId

---
+++ backend/Controllers/OrgEvents.js
@@ -0,0 +1,15 @@
+// This file returns the events created by org   
+ 
+const getMyEvents = async (req, res) => {
+  try {
+    const events = await Event.find({
+      organizer: req.organizerId
+    });
+
+    res.status(200).json({ success: true, events });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+}; 
+ 
+export default getMyEvents;
