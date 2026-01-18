577307666accef7c18ac5b674a6770bfa53a3ce0
Abhishek Yadav <yabhishekk480@gmail.com>
2026-01-18 09:53:20 +0530

    Changed Routes and Org UI

--- frontend/Organizer/Orgdashboard.jsx
+++ frontend/Organizer/Orgdashboard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
+import Events from "./Events";
+import { CiSquarePlus } from "react-icons/ci";
 const Orgdashboard = () => { 
     
   const navigate=useNavigate();
@@ -10,10 +11,15 @@ const Orgdashboard = () => {
   
     <div >
 
-      <div className="md:text-2xl m-5  text-center " >Organizer Dashboard</div> 
-      <div> 
-         <button onClick={()=>navigate("/create-event")} className=" md:w-3/4 p-3 m-4   " > Create a New Event  </button>  
-      </div>
+      <div className="md:text-2xl m-5   text-center " >Organizer Dashboard</div> 
+      <div className="flex  justify-center items-center   " > 
+         <button onClick={()=>navigate("/create-event")} className="flex items-center justify-center gap-2 w-lg bg-blue-700 text-white px-8 py-4 m-4 rounded-3xl text-lg font-semibold hover:bg-blue-900" >  
+           Create a New Event  
+           <CiSquarePlus size={30} />
+         </button>  
+         
+      </div> 
+      < Events />
     </div> </> 
 };
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Userlogin from "../User/Userlogin";
-  
 import Usersignup from "../User/Usersignup";
-import Orgsignup from "../Organizer/Orgsignup" 
+import Orgsignup from "../Organizer/Orgsignup"
 import Orglogin from "../Organizer/Orglogin"
 import { ToastContainer, toast } from 'react-toastify';
 import Orgdashboard from "../Organizer/Orgdashboard";
@@ -13,24 +11,32 @@ import Createevent from "../Organizer/Createevent";
 import Allevent from "../Organizer/Events";
 import Events from "../Organizer/Events";
 
-const App = () => {
+const App = () => { 
+   
+ 
+  
+
+   
   return (
-    <BrowserRouter> 
-  <ToastContainer  position="top-right"
-autoClose={1000} />
-      <Routes> 
-        
+    <BrowserRouter>
+      <ToastContainer position="top-right"
+        autoClose={1000} />
+      <Routes>
+
+        {/* user Routes */}
+
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} /> 
-        <Route path="/user-signup" element= { <Usersignup/> } />
-        <Route path="/user-login" element= { <Userlogin/> } /> 
-         <Route path="/organizer-signup" element= { < Orgsignup /> } />
-        <Route path="/organizer-login" element= { <  Orglogin  /> } /> 
-       <Route path="/organizer-dashboard" element={ <Orgdashboard /> }  /> 
-         <Route path="/create-event" element={ <Createevent /> }  /> 
-        <Route path="/all-events" element={ <Allevent /> }  /> 
-         <Route path="/org-events" element={ <Events /> }  /> 
-        
+        <Route path="/user-signup" element={<Usersignup />} />
+        <Route path="/user-login" element={<Userlogin />} />
+        <Route path="/all-events" element={  <  Allevent   />} />
+
+        {/* org routes */}
+
+        <Route path="/organizer-signup" element={< Orgsignup />} />
+        <Route path="/organizer-login" element={<  Orglogin />} />
+        <Route path="/organizer-dashboard" element={<Orgdashboard />} />
+        <Route path="/create-event" element={<Createevent />} />
+
       </Routes>
     </BrowserRouter>
   );
